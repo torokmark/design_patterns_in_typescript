9414fbc3b6e0da1532b109e979c192b92d136b83
torokmark <torok.marko@gmail.com>
2013-07-23 15:46:52 +0200

    Added observer pattern

---
+++ observer/README.md
@@ -0,0 +1,5 @@
+## Observer Pattern ##
+
+Define a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically. 
+
+*GOF*
---
+++ observer/observer.ts
@@ -0,0 +1,80 @@
+class Subject {
+    private observers: Observer[] = [];
+
+    public register(observer: Observer): void {
+        console.log(observer, "is pushed!");
+        this.observers.push(observer);
+    }
+
+    public unregister(observer: Observer): void {
+        var n: number = this.observers.indexOf(observer);
+        console.log(observer, "is removed");
+        this.observers.splice(n, 1);
+    }
+
+    public notify(): void {
+        console.log("notify all the observers", this.observers);
+        var i: number
+          , max: number;
+
+        for (i = 0, max = this.observers.length; i < max; i += 1) {
+            this.observers[i].notify();
+        }
+    }
+}
+
+class ConcreteSubject extends Subject {
+    private subjectState: number;
+
+    get SubjectState(): number {
+        return this.subjectState;
+    }
+
+    set SubjectState(subjectState: number) {
+        this.subjectState = subjectState;
+    }
+}
+
+class Observer {
+    public notify(): void {
+        throw new Error("Abstract Method!");
+    }
+}
+
+class ConcreteObserver extends Observer {
+    private name: string;
+    private state: number;
+    private subject: ConcreteSubject;
+
+    constructor (subject: ConcreteSubject, name: string) {
+        super();
+        console.log("ConcreteObserver", name, "is created!");
+        this.subject = subject;
+        this.name = name;
+    }
+
+    public notify(): void {
+        console.log("ConcreteObserver's notify method");
+        console.log(this.name, this.state);
+        this.state = this.subject.SubjectState;
+    }
+
+    get Subject(): ConcreteSubject {
+        return this.subject;
+    }
+
+    set Subject(subject: ConcreteSubject) {
+        this.subject = subject;
+    }
+}
+
+(function main() {
+    var sub: ConcreteSubject = new ConcreteSubject();
+
+    sub.register(new ConcreteObserver(sub, "Jancsi"));
+    sub.register(new ConcreteObserver(sub, "Julcsa"));
+    sub.register(new ConcreteObserver(sub, "Marcsa"));
+
+    sub.SubjectState = 123;
+    sub.notify();
+}());
