909af0abbda6cbb0951fa7c5ac9755e018ad51ff
Vo Tuan Trung <trung.vo@zyllem.com>
2016-12-28 11:38:10 +0800

    Update concrete class to implement the Element interface
    Then in demo.ts no need to cast from Concrete Element to Element

--- visitor/demo.ts
+++ visitor/demo.ts
@@ -5,8 +5,8 @@ namespace VisitorPattern {
 		export function show() : void {
 		    var objs: VisitorPattern.Objs = new VisitorPattern.Objs();
 
-			objs.attach(<VisitorPattern.Element> new VisitorPattern.ConcreteElement1());
-			objs.attach(<VisitorPattern.Element> new VisitorPattern.ConcreteElement2());
+			objs.attach(new VisitorPattern.ConcreteElement1());
+			objs.attach(new VisitorPattern.ConcreteElement2());
 
 			var v1: VisitorPattern.ConcreteVisitor1 = new VisitorPattern.ConcreteVisitor1(),
 				v2: VisitorPattern.ConcreteVisitor2 = new VisitorPattern.ConcreteVisitor2();
