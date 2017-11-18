0e2785e0afb2a18b7dc7881286e76ed12dddee0c
Chandan CH <chandanch@users.noreply.github.com>
2017-11-18 18:41:49 +0530

    new approach for singleton

--- singleton/src/singleton.ts
+++ singleton/src/singleton.ts
@@ -1,14 +1,24 @@
-namespace SingletonPattern {
-    export class Singleton {
-        private static instance: Singleton;
-
-        constructor() {}
-
-        static get Instance() {
-            if (this.instance === null || this.instance === undefined) {
-                this.instance = new Singleton();
+namespace Singleton {
+    class Singleton {
+        // A variable which stores the singleton object. Intially,
+        // the variable acts like a placeholder
+        private static __singleton: Singleton = null;
+        // private constructor so that no instance is created
+        private constructor() {
+        }
+        // This is how we create a singleton object
+        public static getInstance(): Singleton {
+            // check if an instance of the class is already created
+            if (this.__singleton == null) {
+                // If not created create an instance of the class
+                // store the instance in the variable
+                this.__singleton = new Singleton();
             }
-            return this.instance;
+            // return the singleton object
+            return this.__singleton
+        }
+        public getSingletonName(name: string) {
+            alert(`Singleton is ${name}`)
         }
     }
 }
