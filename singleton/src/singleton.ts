35ac2d2392c33becf3f15665e70184480dafb9d8
Márk Török <torokmark@users.noreply.github.com>
2017-12-05 16:14:16 +0100

    rename instance variable

--- singleton/src/singleton.ts
+++ singleton/src/singleton.ts
@@ -2,20 +2,20 @@ namespace Singleton {
     class Singleton {
         // A variable which stores the singleton object. Intially,
         // the variable acts like a placeholder
-        private static __singleton: Singleton = null;
+        private static singleton: Singleton = null;
         // private constructor so that no instance is created
         private constructor() {
         }
         // This is how we create a singleton object
         public static getInstance(): Singleton {
             // check if an instance of the class is already created
-            if (this.__singleton == null) {
+            if (this.singleton == null) {
                 // If not created create an instance of the class
                 // store the instance in the variable
-                this.__singleton = new Singleton();
+                this.singleton = new Singleton();
             }
             // return the singleton object
-            return this.__singleton
+            return this.singleton
         }
     }
 }
