5a943b7f2f2ee38ebfb97c3414b96aea58043af4
Grigory Panov <gripanov@gmail.com>
2018-06-19 09:07:41 +0300

    Support naming convention and typescript features

--- singleton/singleton.ts
+++ singleton/singleton.ts
@@ -2,14 +2,16 @@ namespace SingletonPattern {
     export class Singleton {
         // A variable which stores the singleton object. Intially,
         // the variable acts like a placeholder
-        private static singleton: Singleton = null;
+        private static singleton: Singleton;
+
         // private constructor so that no instance is created
         private constructor() {
         }
+
         // This is how we create a singleton object
-        public static Instance(): Singleton {
+        public static getInstance(): Singleton {
             // check if an instance of the class is already created
-            if (this.singleton == null) {
+            if (!this.singleton) {
                 // If not created create an instance of the class
                 // store the instance in the variable
                 this.singleton = new Singleton();
