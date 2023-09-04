e50b5994e019f41a322ed2b74f4078065514b2a9
Israel Borba <israel.borba@gmail.com>
2023-09-04 16:44:02 -0300

    create an extra sample

--- creational/builder/demo.ts
+++ creational/builder/demo.ts
@@ -3,12 +3,18 @@
 namespace BuilderPattern {
 	export namespace Demo {
 		export function show() : void {
-			var u: BuilderPattern.User = new BuilderPattern.UserBuilder("Jancsi")
+			const u: BuilderPattern.User = new BuilderPattern.UserBuilder("Doe")
                         .setAge(12)
                         .setPhone("0123456789")
                         .setAddress("asdf")
                         .build();
     		console.log(u.Name + " " + u.Age + " " + u.Phone + " " + u.Address);
+
+				const u2: BuilderPattern.User = new BuilderPattern.UserBuilder("John")
+													.setAge(18)
+													.setPhone("987654321")
+													.build();
+					console.log(u2.Name + " " + u2.Age + " " + u2.Phone + " " + u2.Address);
 		}
 	}
 }
