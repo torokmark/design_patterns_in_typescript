365c486892607b8ef8d0b92494f1db238cfaa669
Israel Borba <israel.borba@gmail.com>
2023-09-04 16:43:52 -0300

    update creational var from const

--- creational/abstract_factory/demo.ts
+++ creational/abstract_factory/demo.ts
@@ -4,13 +4,13 @@ namespace AbstractFactoryPattern {
 	export namespace Demo {
 		export function show() {
     		// Abstract factory1
-		    var factory1: AbstractFactoryPattern.AbstractFactory = new AbstractFactoryPattern.ConcreteFactory1();
-    		var tester1: AbstractFactoryPattern.Tester = new AbstractFactoryPattern.Tester(factory1);
+		    const factory1: AbstractFactoryPattern.AbstractFactory = new AbstractFactoryPattern.ConcreteFactory1();
+    		const tester1: AbstractFactoryPattern.Tester = new AbstractFactoryPattern.Tester(factory1);
 		    tester1.test();
 
 		    // Abstract factory2
-		    var factory2: AbstractFactoryPattern.AbstractFactory = new AbstractFactoryPattern.ConcreteFactory2();
-			var tester2: AbstractFactoryPattern.Tester = new AbstractFactoryPattern.Tester(factory2);
+		    const factory2: AbstractFactoryPattern.AbstractFactory = new AbstractFactoryPattern.ConcreteFactory2();
+				const tester2: AbstractFactoryPattern.Tester = new AbstractFactoryPattern.Tester(factory2);
 		    tester2.test();
 		}
 	}
--- creational/factory_method/demo.ts
+++ creational/factory_method/demo.ts
@@ -3,8 +3,8 @@
 namespace FactoryMethodPattern {
 	export namespace Demo {
 		export function show() : void {
-		    var a: FactoryMethodPattern.AbstractProduct = FactoryMethodPattern.ProductFactory.createProduct("A");
-		    var b: FactoryMethodPattern.AbstractProduct = FactoryMethodPattern.ProductFactory.createProduct("B");
+		    const a: FactoryMethodPattern.AbstractProduct = FactoryMethodPattern.ProductFactory.createProduct("A");
+		    const b: FactoryMethodPattern.AbstractProduct = FactoryMethodPattern.ProductFactory.createProduct("B");
 
 		    console.log(a.method());
 		    console.log(b.method());
